8de2a768f9e067b7a058010d8abfd16fc40b0aee
Marcus Vinícius <marcus-vnt@live.com>
2025-02-17 16:27:25 -0300

    First Commit

---
+++ src/app/(site)/(home)/page.tsx
@@ -0,0 +1,5 @@
+import Home from "@/app/components/pages/home";
+
+export default function HomePage() {
+  return <Home />;
+}
---
+++ src/app/components/pages/home/index.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Olá, milionário!</h1>
+      <h2>Hoje você já lucrou 5.345,30 dólares.</h2>
+    </div>
+  );
+}
