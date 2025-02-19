787e8f482fdd862c79bdd123874708171136d9a3
Marcus Vinícius <marcus-vnt@live.com>
2025-02-19 13:45:53 -0300

    Part One.

--- src/app/components/pages/home/index.tsx
+++ src/app/components/pages/home/index.tsx
@@ -1,11 +1,13 @@
 import Header from "./header";
 import Hero from "./hero";
+import PartOne from "./part-one";
 
 export default function Home() {
   return (
     <div className="min-h-screen">
       <Header />
-      <Hero />;
+      <Hero />
+      <PartOne />
     </div>
   );
 }
