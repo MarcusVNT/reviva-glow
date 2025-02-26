f329f8ba8ba1cfd1c500b351c7c4e49a49af0761
Marcus Vinícius <marcus-vnt@live.com>
2025-02-26 12:59:07 -0300

    Part four - 1.

---
+++ src/app/components/pages/home/part-four.tsx
@@ -0,0 +1,35 @@
+export default function PartFour() {
+  return (
+    <div className="max-w-7xl mx-auto py-4 px-8 flex flex-col  justify-between items-center">
+      <div className="flex items-center">
+        <h2 className="text-3xl font-bold text-gray-900 mb-8 flex items-start">
+          The Silent Revolution Sweeping Across America
+        </h2>
+      </div>
+      <div className="flex flex-col items-start mb-8">
+        <p className="text-lg text-gray-700 mb-2">
+          Mary Thompson, 58, from Connecticut, was one of the study
+          participants.
+        </p>
+        <p className="text-lg text-gray-700 mb-2">
+          "When they told me my skin showed markers of someone in their 30s, I
+          couldn't believe it. But the lab results don't lie."
+        </p>
+        <p className="text-lg text-gray-700 mb-2">And Mary isn't alone.</p>
+        <p className="text-lg text-gray-700 mb-2">
+          Hundreds of women who've discovered how to activate this "youth code"
+          are experiencing similar results:
+        </p>
+        <ul className="list-disc list-inside pl-4 text-lg text-gray-700">
+          <li className="mb-2">Only work on the surface level.</li>
+          <li className="mb-2">Don't deliver essential nutrients.</li>
+          <li className="mb-2">Mask problems instead of solving them.</li>
+        </ul>
+        <p className="text-lg text-gray-700 mb-2 font-semibold">
+          But the real question is: why are some women naturally reversing aging
+          while others aren't?
+        </p>
+      </div>
+    </div>
+  );
+}
