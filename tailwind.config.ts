d74d6a11342cd43cf949c1d45c0ede1c4785d3e8
Marcus Vinícius <marcus-vnt@live.com>
2025-03-12 16:20:41 -0300

    Part seven.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,6 +16,30 @@ module.exports = withMT({
         "orange-one": "#ea6848",
         "orange-two": "#eda78e",
       },
+      keyframes: {
+        "bounce-right": {
+          "0%, 100%": { transform: "translateX(0)" },
+          "50%": { transform: "translateX(25%)" },
+        },
+        "bounce-left": {
+          "0%, 100%": { transform: "translateX(0)" },
+          "50%": { transform: "translateX(-25%)" },
+        },
+        "bounce-diagonal-right": {
+          "0%, 100%": { transform: "translate(0, 0)" },
+          "50%": { transform: "translate(25%, 25%)" },
+        },
+        "bounce-diagonal-left": {
+          "0%, 100%": { transform: "translate(0, 0)" },
+          "50%": { transform: "translate(-25%, 25%)" },
+        },
+      },
+      animation: {
+        "bounce-right": "bounce-right 1s infinite",
+        "bounce-left": "bounce-left 1s infinite",
+        "bounce-diagonal-right": "bounce-diagonal-right 1s infinite",
+        "bounce-diagonal-left": "bounce-diagonal-left 1s infinite",
+      },
     },
   },
   plugins: [require("tailwindcss-animate")],
